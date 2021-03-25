929d44726dcc55244259d86fa5a701a05f12dfee
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2021-03-25 11:47:59 +0530

    commit

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+    const observer = new MutationObserver((mutationRecordList) => {
+        for (const mutationRecord of mutationRecordList) {
+            console.log(mutationRecord.attributeName, ':',mutationRecord.oldValue,'->>' ,mutationRecord.target.getAttribute(mutationRecord.attributeName));
+        }
+    });
+
+    observer.observe(document.body, {
+        subtree: true,
+        attributes: true,
+        attributeOldValue: true,
+    });
+
+    setInterval(()=>{
+        const elem = document.getElementById('content-div-3-p');
+        elem.setAttribute('class', 'shubham');
+    }, 1000);
+});
