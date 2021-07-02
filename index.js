63218e2909ee946361584f69a2e6094665f8946a
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2021-07-02 11:52:45 +0530

    init

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var originalEventListener = EventTarget.prototype.addEventListener;
+if (!originalEventListener) {
+    var originalEventListener = EventTarget.prototype.addEventListener;
+}
+
 EventTarget.prototype.addEventListener = function (type, fn, capture) {
     this.originalEventListener = originalEventListener;
     this.originalEventListener(type, fn, capture);
-    console.log('Shubham Event Listener', type, fn, capture);
-}
+    if (this instanceof Element) {
+        console.log('Shubham Event Listener', this, type, this.onClick);
+    }
+};
