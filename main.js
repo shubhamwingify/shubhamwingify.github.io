468d661586cd1dde2c87ca160c5c8513333f5f77
Shubham Aggarwal <shubham.aggarwal@wingify.com>
2021-11-17 15:24:51 +0530

    init

--- main.js
+++ main.js
@@ -1,28 +1,5 @@
-const elem = document.getElementById('shubham-btn');
-elem.addEventListener('click', function (event) {
-    console.log('addEventListener Click', this);
-});
-
-
-const jqElem = $('#shubham-btn');
-jqElem.click(function () {
-    console.log('Jquery Click', this);
-    debugger;
-});
-
-
-setTimeout(()=> {
-    const elementA = document.getElementById('shubham-btn');
-    const event = new Event("click");
-    const elementB = document.getElementById('shubham-btn-new');
-    // Events attached using overridden addEventListeners
-    const handlers = elementA.__vwo_events;
-    for(const handler of handlers){
-        handler.handler.call(elementB, event);
-    }
-
-    // const elementA = document.getElementById('shubham-btn');
-    // const event = new Event("click");
-    // const elementB = document.getElementById('shubham-btn-new');
-    // elementA.dispatchEvent.call(elementB, event);
-}, 4000);
+const originalAddEventListener = window.addEventListener;
+window.addEventListener = function () {
+    console.log('Overridden add event listener');
+    originalAddEventListener.apply(window, arguments);
+}
